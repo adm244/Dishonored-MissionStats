6930826a4c6965c8092e0dbaca55decd84fb27a5
Adam Worgen <adm2444@gmail.com>
2022-12-16 18:36:42 +0300

    Hotfix: fixed support for update13 This is a temporary solution since this breaks support of previous game version

--- code/game/pointers.h
+++ code/game/pointers.h
@@ -63,63 +63,63 @@ OTHER DEALINGS IN THE SOFTWARE.
 #define STDCALL_FUNCTION(func) CDECL_FUNCTION(func)
 
 //------------- Unreal.h -------------//
-DEFINE_STATIC_POINTER(world, 0x0143D878);
+DEFINE_STATIC_POINTER(world, 0x01449888);
 
-DEFINE_CDECL_FUNCTION(NewObject, 0x00494C80);
-DEFINE_MEMBER_FUNCTION(UClass, GetDefaultObject, 0x004967F0);
-DEFINE_MEMBER_FUNCTION(World, GetGameInfo, 0x007816E0);
+DEFINE_CDECL_FUNCTION(NewObject, 0x004955a0);
+DEFINE_MEMBER_FUNCTION(UClass, GetDefaultObject, 0x00497130);
+DEFINE_MEMBER_FUNCTION(World, GetGameInfo, 0x00781020);
 
 //------------- Dishonored.h -------------//
-DEFINE_STATIC_POINTER(dishonoredEngine, 0x0143B20C);
-DEFINE_STATIC_POINTER(playerController, 0x01452DD4);
-DEFINE_STATIC_POINTER(playerPawn, 0x01452DE8);
+DEFINE_STATIC_POINTER(dishonoredEngine, 0x0144721c);
+DEFINE_STATIC_POINTER(playerController, 0x0145f614);
+DEFINE_STATIC_POINTER(playerPawn, 0x0145f628);
 
-DEFINE_CDECL_FUNCTION(GetGlobalUIManager, 0x00BBF730);
-DEFINE_CDECL_FUNCTION(IsDLC06, 0x00BBF1D0);
-DEFINE_CDECL_FUNCTION(IsDLC07, 0x00BBF270);
-DEFINE_STDCALL_FUNCTION(GetHUD, 0x00BBF760);
-DEFINE_MEMBER_FUNCTION(DishonoredPlayerPawn, HasCompleteAction, 0x00AB1100);
+DEFINE_CDECL_FUNCTION(GetGlobalUIManager, 0x00bbf700);
+DEFINE_CDECL_FUNCTION(IsDLC06, 0x00bbf110);
+DEFINE_CDECL_FUNCTION(IsDLC07, 0x00bbf1b0);
+DEFINE_STDCALL_FUNCTION(GetHUD, 0x00bbf730);
+DEFINE_MEMBER_FUNCTION(DishonoredPlayerPawn, HasCompleteAction, 0x00ab1370);
 
 //------------- MissionStats.h -------------//
-DEFINE_MEMBER_FUNCTION(DisStoryFlagSet, GetStoryFlagSet, 0x00C04640);
-DEFINE_MEMBER_FUNCTION(DisDarknessManager, GetChaosTresholdIndex, 0x00C47FF0);
-DEFINE_MEMBER_FUNCTION(DishonoredPlayerPawn, GetStatsValue, 0x00AA93C0);
-DEFINE_MEMBER_FUNCTION(ArkProfileSettings, SetMissionStats, 0x00938210);
+DEFINE_MEMBER_FUNCTION(DisStoryFlagSet, GetStoryFlagSet, 0x00c049f0);
+DEFINE_MEMBER_FUNCTION(DisDarknessManager, GetChaosTresholdIndex, 0x00c487d0);
+DEFINE_MEMBER_FUNCTION(DishonoredPlayerPawn, GetStatsValue, 0x00aa9700);
+DEFINE_MEMBER_FUNCTION(ArkProfileSettings, SetMissionStats, 0x009376d0);
 
 //------------- MoviePlayer.h -------------//
-DEFINE_MEMBER_FUNCTION(DisGFxMoviePlayerHUD, ShowLocationDiscovery, 0x00B94D60);
-DEFINE_MEMBER_FUNCTION(DisGFxMoviePlayerHUD, ShowGameMessage, 0x00BAFFA0);
-DEFINE_MEMBER_FUNCTION(DisGFxMoviePlayerMissionStats, Show, 0x00BD3A60);
-//DEFINE_MEMBER_FUNCTION(Unk20, ShowNote, 0x00BCC2B0);
+DEFINE_MEMBER_FUNCTION(DisGFxMoviePlayerHUD, ShowLocationDiscovery, 0x00b95160);
+DEFINE_MEMBER_FUNCTION(DisGFxMoviePlayerHUD, ShowGameMessage, 0x00bb0140);
+DEFINE_MEMBER_FUNCTION(DisGFxMoviePlayerMissionStats, Show, 0x00bd3b20);
+//DEFINE_MEMBER_FUNCTION(Unk20, ShowNote, 0x00bcc390);
 
 //------------- Scaleform3.h -------------//
-DEFINE_MEMBER_FUNCTION(GFxValue, SetUndefined, 0x0097B650);
-DEFINE_MEMBER_FUNCTION(GFxValue, SetNull, 0x0097B680);
-DEFINE_MEMBER_FUNCTION(GFxValue, SetBoolean, 0x0097B6B0);
-DEFINE_MEMBER_FUNCTION(GFxValue, SetDouble, 0x0097B700);
-DEFINE_MEMBER_FUNCTION(GFxValue, SetStringW, 0x0097B740);
+DEFINE_MEMBER_FUNCTION(GFxValue, SetUndefined, 0x0097aac0);
+DEFINE_MEMBER_FUNCTION(GFxValue, SetNull, 0x0097aaf0);
+DEFINE_MEMBER_FUNCTION(GFxValue, SetBoolean, 0x0097ab20);
+DEFINE_MEMBER_FUNCTION(GFxValue, SetDouble, 0x0097ab70);
+DEFINE_MEMBER_FUNCTION(GFxValue, SetStringW, 0x0097abb0);
 
-DEFINE_MEMBER_FUNCTION(ValueInterface, GetMember, 0x00DA8AD0);
-DEFINE_MEMBER_FUNCTION(ValueInterface, SetMember, 0x00DA58F0);
-DEFINE_MEMBER_FUNCTION(ValueInterface, Invoke, 0x00DA8230);
-DEFINE_MEMBER_FUNCTION(ValueInterface, PushBack, 0x00DA5A20);
-DEFINE_MEMBER_FUNCTION(ValueInterface, PopBack, 0x00DA68C0);
+DEFINE_MEMBER_FUNCTION(ValueInterface, GetMember, 0x00da9470);
+DEFINE_MEMBER_FUNCTION(ValueInterface, SetMember, 0x00da6290);
+DEFINE_MEMBER_FUNCTION(ValueInterface, Invoke, 0x00da8bd0);
+DEFINE_MEMBER_FUNCTION(ValueInterface, PushBack, 0x00da63c0);
+DEFINE_MEMBER_FUNCTION(ValueInterface, PopBack, 0x00da7260);
 
 //------------- MissionStatsButton.cpp -------------//
-DEFINE_STATIC_POINTER(detour_showpausemenu, 0x00BCCCA0);
-DEFINE_STATIC_POINTER(detour_setmissionstats, 0x00BD3BE0);
-DEFINE_STATIC_POINTER(detour_shouldskipmustdoflag, 0x00BD42F1);
-DEFINE_STATIC_POINTER(detour_distweaks_missionstats_ctor, 0x00A0DD50);
-DEFINE_STATIC_POINTER(detour_distweaks_missionstats_dtor, 0x00A0A150);
+DEFINE_STATIC_POINTER(detour_showpausemenu, 0x00bccd80);
+DEFINE_STATIC_POINTER(detour_setmissionstats, 0x00bd3ca0);
+DEFINE_STATIC_POINTER(detour_shouldskipmustdoflag, 0x00bd43b1);
+DEFINE_STATIC_POINTER(detour_distweaks_missionstats_ctor, 0x00a0d930);
+DEFINE_STATIC_POINTER(detour_distweaks_missionstats_dtor, 0x00a09ce0);
 
-DEFINE_STATIC_POINTER(hook_showpausemenu_ret, 0x00BCCCA5);
-DEFINE_STATIC_POINTER(hook_setmissionstats_ret, 0x00BD3BE6);
-DEFINE_STATIC_POINTER(hook_shouldskipmustdoflag_ret, 0x00BD42F7);
-DEFINE_STATIC_POINTER(hook_distweaks_missionstats_ctor_ret, 0x00A0DD57);
-DEFINE_STATIC_POINTER(hook_distweaks_missionstats_dtor_ret, 0x00A0A155);
+DEFINE_STATIC_POINTER(hook_showpausemenu_ret, 0x00bccd85);
+DEFINE_STATIC_POINTER(hook_setmissionstats_ret, 0x00bd3ca6);
+DEFINE_STATIC_POINTER(hook_shouldskipmustdoflag_ret, 0x00bd43b7);
+DEFINE_STATIC_POINTER(hook_distweaks_missionstats_ctor_ret, 0x00a0d937);
+DEFINE_STATIC_POINTER(hook_distweaks_missionstats_dtor_ret, 0x00a09ce5);
 
 //------------- MissionStatsGuard.cpp -------------//
-DEFINE_STATIC_POINTER(detour_modifystatvariable, 0x00AA94A0);
-DEFINE_STATIC_POINTER(hook_modifystatvariable_ret, 0x00AA94A5);
+DEFINE_STATIC_POINTER(detour_modifystatvariable, 0x00aa97e0);
+DEFINE_STATIC_POINTER(hook_modifystatvariable_ret, 0x00aa97e5);
 
 #endif
